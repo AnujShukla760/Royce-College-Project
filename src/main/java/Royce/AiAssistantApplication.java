957487e046a83f7a68af5b52fd0ba10cba744513
AnujShukla760 <shuklaanuj312@gmail.com>
2025-04-28 14:04:55 +0530

    first commit

---
+++ src/main/java/Royce/AiAssistantApplication.java
@@ -0,0 +1,14 @@
+package Royce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AiAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Royce/AiAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package Royce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
